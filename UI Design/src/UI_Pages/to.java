f01f6dfc1978fb6143a3043545392e5767ce1509
Njidda Salifu <njiddasalifu@gmail.com>
2022-12-08 08:42:01 +0100

    Some names changed and project srtucture refactor for better collaboration

---
+++ UI Design/src/UI_Pages/to.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UI_Pages;
+
+/**
+ *
+ * @author Windows
+ */
+class to {
+    
+}
